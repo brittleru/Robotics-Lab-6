05dbb3f7c0b7bcf55bb6266ecf1ff3f4e5fce3d1
Sebastian Mocanu <diemetal851@gmail.com>
2020-12-18 00:27:56 +0200

    second commit

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const toRadians = angle => angle * (PI / 180);
 
 
 // Global variables
-const lengthL1 = 60;       // [m]
-const lengthL2 = 60;       // [m]
-const a = 10;                // [m]
-const xi = 10;               // [m]
-const xf = 40;               // [m]
-const yi = 50;               // [m]
-const yf = 10;               // [m]
+const lengthL1 = 60;    // [mm]
+const lengthL2 = 60;    // [mm]
+const a = 10;           // [mm]
+const xi = 10;          // [mm]
+const xf = 40;          // [mm]
+const yi = 50;          // [mm]
+const yf = 10;          // [mm]
 
 
 // Result arrays
@@ -39,23 +39,72 @@ for (let i = 10; i <= 50; i += 4) {
   yArr.unshift(i);
 }
 
-// console.log(xArr);
-// console.log(yArr);
-
-
-
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i <= 10; i++) {
 
   let rho1 = Math.sqrt(Math.pow(a + xArr[i], 2) + Math.pow(yArr[i], 2));
   let alpha1 = Math.atan(yArr[i] / (a + xArr[i]));
   let h1 = Math.sqrt((lengthL1 + lengthL2 + rho1) * (lengthL1 + lengthL2 - rho1) *
-                     (lengthL2 + rho1 - lengthL1) * (rho1 + lengthL1 - lengthL2) /
-                     (2 * rho1));
+                     (lengthL2 + rho1 - lengthL1) * (rho1 + lengthL1 - lengthL2))
+                     / (2 * rho1);
   let t1 = rho1 - Math.sqrt(Math.pow(lengthL2, 2) - Math.pow(h1, 2));
-  
+  let beta1 = Math.atan(h1 / t1);
+  let phi1 = alpha1 + beta1;
 
+  let rho2 = Math.sqrt(Math.pow(a - xArr[i], 2) + Math.pow(yArr[i], 2));
+  let alpha2 = Math.atan(yArr[i] / (a - xArr[i]));
+  let h2 = Math.sqrt((lengthL1 + lengthL2 + rho2) * (lengthL1 + lengthL2 - rho2) *
+                     (lengthL2 + rho2 - lengthL1) * (rho2 + lengthL1 - lengthL2))
+                     / (2 * rho2);
+  let t2 = rho2 - Math.sqrt(Math.pow(lengthL2, 2) - Math.pow(h2, 2));
+  let beta2 = Math.atan(h2 / t2);
+  let phi2 = alpha2 + beta2;
 
+  // Update DOM elements
+  this[`x${i}`] = document.getElementById(`x-${i}`);
+  let xDom = this[`x${i}`];
+  xDom.innerHTML = xArr[i];
+
+  this[`y${i}`] = document.getElementById(`y-${i}`);
+  let yDom = this[`y${i}`];
+  yDom.innerHTML = yArr[i];
+
+  this[`phi1_${i}`] = document.getElementById(`phi1-${i}`);
+  let phi1Dom = this[`phi1_${i}`];
+  phi1Dom.innerHTML = toDegrees(phi1);
+
+  this[`phi2_${i}`] = document.getElementById(`phi2-${i}`);
+  let phi2Dom = this[`phi2_${i}`];
+  phi2Dom.innerHTML = toDegrees(phi2);
+
+  // Fill arrays
+  Rho1Arr.push(rho1);
+  Rho2Arr.push(rho2);
+  Alpha1Arr.push(alpha1);
+  Alpha2Arr.push(alpha2);
+  H1Arr.push(h1);
+  H2Arr.push(h2);
+  T1Arr.push(t1);
+  T2Arr.push(t2);
+  Beta1Arr.push(beta1);
+  Beta2Arr.push(beta2);
+  phi1Arr.push(toDegrees(phi1))
+  phi2Arr.push(toDegrees(phi2))
 }
+
+
+// For higher calculus precision
+console.log("Rho1Arr = ", Rho1Arr);
+console.log("Rho2Arr = ", Rho2Arr);
+console.log("Alpha1Arr = ", Alpha1Arr);
+console.log("Alpha2Arr = ", Alpha2Arr);
+console.log("H1Arr = ", H1Arr);
+console.log("H2Arr = ", H2Arr);
+console.log("T1Arr = ", T1Arr);
+console.log("T2Arr = ", T2Arr);
+console.log("Beta1Arr = ", Beta1Arr);
+console.log("Beta2Arr = ", Beta2Arr);
+console.log("phi1Arr = ", phi1Arr);
+console.log("phi2Arr = ", phi2Arr);
 
 
 
